ba1636ae10d391c0e1bed2bbd623ad544f78722e
Andy <3021778410101@ingenieria.usac.edu.gt>
2025-01-15 01:29:04 -0600

    [FEAT] creacion de filtro busqueda

--- pizzas/src/app/vistas/mostrarpizzas/mostrarpizzas.component.ts
+++ pizzas/src/app/vistas/mostrarpizzas/mostrarpizzas.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 import { ApiService } from '../../services/api.service';
 
 @Component({
@@ -8,21 +14,41 @@ import { ApiService } from '../../services/api.service';
   standalone: true,
   imports: [
     MatTableModule,
-    MatDividerModule
-
+    MatPaginatorModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './mostrarpizzas.component.html',
   styleUrl: './mostrarpizzas.component.css'
 })
-export class MostrarpizzasComponent {
+export class MostrarpizzasComponent implements OnInit, AfterViewInit{
   pedidos: any[] = [];
   displayedColumns: string[] = [ 'cliente', 'producto', 'cantidad', 'fecha'];
+  dataSource = new MatTableDataSource<any>();
 
-  constructor(private api: ApiService) {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-    this.api.listar_pedidos().subscribe((data) => {
-      console.log(data);
-      this.pedidos = data;
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.listar_pedidos().subscribe({
+      next: (data) => {
+        this.dataSource.data = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener pedidos:', err);
+      }
     });
   }
+
+  ngAfterViewInit():void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
